86a4f629d3c5763cfc8e9a2e484055c59e3a6fb6
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2024-12-13 16:53:56 +0530

    added logout middleware

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -33,7 +33,8 @@ const jwt = require('jsonwebtoken');
  })
 
  userSchema.methods.generateAuthToken = function(){
-    const token = jwt.sign({_id:this._id},process.env.JWT_SECRET);
+
+    const token = jwt.sign({_id: this._id}, process.env.JWT_SECRET, { expiresIn: '24h' });
     return token;
  }
 
