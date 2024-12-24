d1d04eea0b321bbed99504079c122e51b8852818
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2024-12-24 16:36:20 +0530

    created location panel

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -1,9 +1,22 @@
-
-
 const LocationSearchPanel = () => {
   return (
     <div>
-        Location Search Panel
+        <div className="flex items-center justify-start gap-4 my-4">
+             <h2 className="bg-[#eee] h-10 w-16 flex items-center justify-center rounded-full"><i className="ri-map-pin-fill"></i></h2>
+             <h4 className="font-medium">Nalanda house boy&apos;s hostel, knowledge park 3, Greater Noida</h4>
+        </div>    
+        <div className="flex items-center justify-start gap-4 my-4">
+             <h2 className="bg-[#eee] h-10 w-16 flex items-center justify-center rounded-full"><i className="ri-map-pin-fill"></i></h2>
+             <h4 className="font-medium">Nalanda house boy&apos;s hostel, knowledge park 3, Greater Noida</h4>
+        </div> 
+        <div className="flex items-center justify-start gap-4 my-4">
+             <h2 className="bg-[#eee] h-10 w-16 flex items-center justify-center rounded-full"><i className="ri-map-pin-fill"></i></h2>
+             <h4 className="font-medium">Nalanda house boy&apos;s hostel, knowledge park 3, Greater Noida</h4>
+        </div> 
+        <div className="flex items-center justify-start gap-4 my-4">
+             <h2 className="bg-[#eee] h-10 w-16 flex items-center justify-center rounded-full"><i className="ri-map-pin-fill"></i></h2>
+             <h4 className="font-medium">Nalanda house boy&apos;s hostel, knowledge park 3, Greater Noida</h4>
+        </div>     
     </div>
   )
 }
