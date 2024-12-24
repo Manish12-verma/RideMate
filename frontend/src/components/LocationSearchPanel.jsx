4b71c14b6dd5a034389a2fe89d939647940a09de
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2024-12-25 01:53:40 +0530

    created choose vehicle feature

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -1,24 +1,28 @@
 const LocationSearchPanel = () => {
+
+
+     // sample of array location 
+
   return (
     <div>
-        <div className="flex items-center justify-start gap-4 my-4">
+        <div className="flex items-center justify-start border-2  border-gray-50 active:border-black gap-4 my-2 p-3 rounded-xl ">
              <h2 className="bg-[#eee] h-10 w-16 flex items-center justify-center rounded-full"><i className="ri-map-pin-fill"></i></h2>
              <h4 className="font-medium">Nalanda house boy&apos;s hostel, knowledge park 3, Greater Noida</h4>
         </div>    
-        <div className="flex items-center justify-start gap-4 my-4">
+        <div className="flex items-center justify-start border-2 border-gray-50 active:border-black gap-4 my-2 p-3 rounded-xl ">
              <h2 className="bg-[#eee] h-10 w-16 flex items-center justify-center rounded-full"><i className="ri-map-pin-fill"></i></h2>
              <h4 className="font-medium">Nalanda house boy&apos;s hostel, knowledge park 3, Greater Noida</h4>
         </div> 
-        <div className="flex items-center justify-start gap-4 my-4">
+        <div className="flex items-center justify-start border-2 border-gray-50 active:border-black gap-4 my-2 p-3 rounded-xl ">
              <h2 className="bg-[#eee] h-10 w-16 flex items-center justify-center rounded-full"><i className="ri-map-pin-fill"></i></h2>
              <h4 className="font-medium">Nalanda house boy&apos;s hostel, knowledge park 3, Greater Noida</h4>
         </div> 
-        <div className="flex items-center justify-start gap-4 my-4">
+        <div className="flex items-center justify-start border-2 border-gray-50 active:border-black gap-4 my-2 p-3 rounded-xl ">
              <h2 className="bg-[#eee] h-10 w-16 flex items-center justify-center rounded-full"><i className="ri-map-pin-fill"></i></h2>
              <h4 className="font-medium">Nalanda house boy&apos;s hostel, knowledge park 3, Greater Noida</h4>
         </div>     
     </div>
-  )
+  ) 
 }
 
 export default LocationSearchPanel
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -38,9 +38,8 @@ const Home = () => {
   }, [panelOpen]);
 
   return (
-    <div className='h-screen relative'>
+    <div className='h-screen relative overflow-hidden'>
       <img className='w-40 absolute left-5 top-5' src={ridemate2} alt='ridemate logo' />
-
       <div className='h-screen w-screen'>
         {/* image for temporary use */}
         <img
@@ -49,7 +48,6 @@ const Home = () => {
           alt='map'
         />
       </div>
-
       <div className='h-screen absolute bottom-0 w-full flex flex-col justify-end'>
         <div className='h-[30%] p-6 bg-white relative'>
           <h5
@@ -83,6 +81,36 @@ const Home = () => {
         <div ref={panelRef} className='h-0 bg-white'>
            <LocationSearchPanel/>
         </div>
+      </div>
+      <div className='fixed z-10 w-full bottom-0 px-3 py-8 bg-white rounded-t-3xl translate-y-full'>
+        <h3 className='text-2xl font-semibold mb-5'>Choose a vehicle</h3>
+             <div className='flex w-full border-2 mb-2 active:border-black bg-gray-50 items-center justify-between p-3 rounded-xl'>
+                <img className='h-12' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_638,w_956/v1555367349/assets/d7/3d4b80-1a5f-4a8b-ac2b-bf6c0810f050/original/Final_XL.png" alt="" />
+                <div className=' w-1/2'>
+                    <h4 className='font-medium text-lg'>RideMate Go <span><i className="ri-user-3-fill"></i>4</span></h4>
+                    <h5 className='font-medium text-sm'>2 mins away</h5>
+                    <p  className='font-normal text-xs text-gray-600'>Affordable, Compact rides</p>
+                </div>
+                    <h2 className='text-lg font-semibold'>₹193.20</h2>
+             </div>
+             <div className='flex w-full border-2 mb-2 active:border-black  bg-gray-50 items-center justify-between p-3 rounded-xl'>
+                <img className='h-12' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png" alt="" />
+                <div className=' w-1/2'>
+                    <h4 className='font-medium text-lg'> Moto <span><i className="ri-user-3-fill"></i>1</span></h4>
+                    <h5 className='font-medium text-sm'>3 mins away</h5>
+                    <p  className='font-normal text-xs text-gray-600'>Affordable motocycle rides</p>
+                </div>
+                    <h2 className='text-lg font-semibold'>₹65.20</h2>
+             </div>
+             <div className='flex w-full border-2 mb-2 active:border-black  bg-gray-50 items-center justify-between p-3 rounded-xl'>
+                <img className='h-12' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsFabRnJZ8deGXJSKA1QjN45920WytRrdFsA&s" alt="" />
+                <div className=' w-1/2'>
+                    <h4 className='font-medium text-lg'>RideMate Auto <span><i className="ri-user-3-fill"></i>3</span></h4>
+                    <h5 className='font-medium text-sm'>2 mins away</h5>
+                    <p  className='font-normal text-xs text-gray-600'>Affordable Auto rides</p>
+                </div>
+                    <h2 className='text-lg font-semibold'>₹113.5</h2>
+             </div>
       </div>
     </div>
   );
